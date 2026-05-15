c6b9ac6958da3b570a204adbc7ab8e052e60b2a2
666hanrui <zhia2805769@gmail.com>
2026-05-16 00:00:33 +0800

    feat(frontend): restore eight primary routes

--- frontend-src/src/App.tsx
+++ frontend-src/src/App.tsx
@@ -5,6 +5,9 @@ import AuthModal from "./components/AuthModal";
 import InspirationHub from "./pages/InspirationHub";
 import WorkflowValley from "./pages/WorkflowValley";
 import AssetsForge from "./pages/AssetsForge";
+import PromptLab from "./pages/PromptLab";
+import SeedancePage from "./pages/SeedancePage";
+import ProjectsPage from "./pages/ProjectsPage";
 import Settings from "./pages/Settings";
 
 function App() {
@@ -16,6 +19,10 @@ function App() {
           <Route index element={<InspirationHub />} />
           <Route path="workflow" element={<WorkflowValley />} />
           <Route path="assets" element={<AssetsForge />} />
+          <Route path="image" element={<PromptLab kind="image" />} />
+          <Route path="video" element={<PromptLab kind="video" />} />
+          <Route path="seedance" element={<SeedancePage />} />
+          <Route path="projects" element={<ProjectsPage />} />
           <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
