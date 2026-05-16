561956793e2883847264712c63006d53301843a2
666hanrui <zhia2805769@gmail.com>
2026-05-16 20:49:43 +0800

    feat(frontend): add route guards to real pages

--- frontend-src/src/App.tsx
+++ frontend-src/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import AuthModal from "./components/AuthModal";
+import RouteGuard from "./components/router/RouteGuard";
 import InspirationHub from "./pages/InspirationHub";
 import WorkflowValley from "./pages/WorkflowValley";
 import ScriptTasksPage from "./pages/ScriptTasksPage";
@@ -18,14 +19,14 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<InspirationHub />} />
-          <Route path="workflow" element={<WorkflowValley />} />
-          <Route path="scripts" element={<ScriptTasksPage />} />
-          <Route path="assets" element={<AssetsForge />} />
-          <Route path="image" element={<PromptLab kind="image" />} />
-          <Route path="video" element={<PromptLab kind="video" />} />
-          <Route path="seedance" element={<SeedancePage />} />
           <Route path="projects" element={<ProjectsPage />} />
+          <Route path="scripts" element={<ScriptTasksPage />} />
           <Route path="settings" element={<Settings />} />
+          <Route path="workflow" element={<RouteGuard requireProjectId><WorkflowValley /></RouteGuard>} />
+          <Route path="assets" element={<RouteGuard requireTaskId><AssetsForge /></RouteGuard>} />
+          <Route path="image" element={<RouteGuard requireTaskId><PromptLab kind="image" /></RouteGuard>} />
+          <Route path="video" element={<RouteGuard requireTaskId><PromptLab kind="video" /></RouteGuard>} />
+          <Route path="seedance" element={<RouteGuard requireTaskId><SeedancePage /></RouteGuard>} />
         </Route>
       </Routes>
     </BrowserRouter>
